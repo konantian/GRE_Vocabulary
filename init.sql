74bd0221334da1585cc7f20cdad040e689e6fa6e
Yuan Wang <wang17@ualberta.ca>
2019-06-04 18:38:19 -0600

    add tables to database

---
+++ init.sql
@@ -0,0 +1,15 @@
+CREATE TABLE yaoniming3000 (
+	id 				   TEXT,
+	word	   		   TEXT,
+	meaning	           TEXT,
+	date_when_inserted DATE,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE other (
+	id 				   TEXT,
+	word	   		   TEXT,
+	meaning	           TEXT,
+	date_when_inserted DATE,
+	PRIMARY KEY (id)
+);
